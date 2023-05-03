01fd86274ffd7d5bb8abcbbb6efb4e7fdd8c1e50
Matias1987 <ch.matias87@gmail.com>
2023-05-03 18:57:03 -0300

    Update global.js
    database name change

--- src/lib/global.js
+++ src/lib/global.js
@@ -9,7 +9,7 @@ const connection_data = {
     user: "admin",
     password: "admin",
     host: "testdatabase.cogxzxmytpda.us-east-1.rds.amazonaws.com",
-    database: "test_optdelsol_23"
+    database: "optdelsol_24"
 
 } 
 
