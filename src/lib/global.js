d6eb3cc880c58fc958cfc2e8e5b6dbe353484024
Bruno <ch.matias87@gmail.com>
2023-09-04 20:28:55 -0300

    varios

--- src/lib/global.js
+++ src/lib/global.js
@@ -1,16 +1,16 @@
-//const connection_data = {
-//    user: "root",
-//    password: "root",
-//    host: "localhost",
-//    database: "optdelsol_28"
-//} 
-
 const connection_data = {
-    user: "admin",
-    password: "admin2023",
-    host: "testdatabase.cogxzxmytpda.us-east-1.rds.amazonaws.com",
-    database: "optdelsol_25"
+    user: "root",
+    password: "root",
+    host: "localhost",
+    database: "optdelsol_29"
 } 
+
+//const connection_data = {
+//    user: "admin",
+//    password: "admin2023",
+//    host: "testdatabase.cogxzxmytpda.us-east-1.rds.amazonaws.com",
+//    database: "optdelsol_25"
+//} 
 
 module.exports = {
     connection_data
