5674c99390700e0f705700126111e4fbedcfe669
matias <devoptdelsol@gmail.com>
2023-09-14 13:01:40 -0300

    varios

--- src/database/Cliente.js
+++ src/database/Cliente.js
@@ -179,31 +179,55 @@ const operaciones_cliente = (idcliente,callback) => {
 
 const obtener_saldo_ctacte = (idcliente,callback) => {
     //FALTAN CONDICIONES EN LA CONSULTA, COMO POR  EJ. TENER EN CTA. QUE LA VENTA NO ESTE ANULADA
-    const query  = `SELECT SUM(op.debe) AS 'debe', SUM(op.haber) AS 'haber' FROM
+    const query  = `SELECT SUM(op.debe) AS 'debe', SUM(op.haber) AS 'haber'  FROM
     (
+    	
         SELECT 
+        CONCAT('a',c.idcobro) as 'id',
         c.monto AS 'haber',
         0 AS 'debe'
-        FROM cobro c,venta_has_modo_pago vmp WHERE 
+        FROM 
+		  cobro c,
+		  venta v,
+		  venta_has_modo_pago vmp 
+		  WHERE 
+		  vmp.venta_idventa = v.idventa AND 
+		  v.estado = 'ENTREGADO' AND 
         vmp.modo_pago = 'ctacte' AND 
         c.venta_idventa = vmp.venta_idventa  AND
         c.tipo <> 'cuota' AND
         c.cliente_idcliente=${idcliente}
         union
-        SELECT 
-        0 AS 'haber', 
-        vmp.monto_cuota * vmp.cant_cuotas AS 'debe'
-        FROM venta_has_modo_pago vmp, venta v WHERE v.idventa = vmp.venta_idventa AND vmp.modo_pago='ctacte' AND v.cliente_idcliente=${idcliente}
-        union
-        SELECT 
-        0 AS 'haber', 
-        cm.monto AS 'debe'
-        FROM carga_manual cm WHERE cm.cliente_idcliente=${idcliente}
-        union
-        SELECT 
+        
+		 
+         SELECT 
+         CONCAT('b',c.idcobro) as 'id',
         c.monto AS 'haber',
         0 AS 'debe'
          FROM cobro c WHERE c.tipo = 'cuota' AND c.cliente_idcliente=${idcliente}
+         union
+         
+         
+        SELECT 
+        CONCAT('c',v.idventa) as 'id',
+        0 AS 'haber', 
+        ((v.monto_total - vmp.monto) + vmp.cant_cuotas * vmp.monto_cuota) AS 'debe'
+        FROM 
+		  venta_has_modo_pago vmp, 
+		  venta v 
+		  WHERE 
+		  v.idventa = vmp.venta_idventa AND 
+		  vmp.modo_pago='ctacte' AND 
+		  v.cliente_idcliente=${idcliente} AND 
+		  v.estado='ENTREGADO'
+        union
+        
+        SELECT 
+        CONCAT('d',cm.idcarga_manual) as 'id',
+        0 AS 'haber', 
+        cm.monto AS 'debe'
+        FROM carga_manual cm WHERE cm.cliente_idcliente=${idcliente}
+        
     ) AS op
     ;`
 
