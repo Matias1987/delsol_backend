2268a095bbcbfb0319c562d949e63735b678cd08
matias <devoptdelsol@gmail.com>
2023-10-17 12:16:22 -0300

    varios

--- src/lib/global.js
+++ src/lib/global.js
@@ -2,7 +2,7 @@ const connection_data = {
     user: "root",
     password: "root",
     host: "localhost",
-    database: "optica_31_backup"//"optdelsol_28"//"optica_30"
+    database: "optica_test17102023"//"optdelsol_28"//"optica_30"
 } 
 
 //const connection_data = {
