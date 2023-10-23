00199e0e14c483496f8a8b8daf7a3bfed54b9c2b
matias <devoptdelsol@gmail.com>
2023-10-23 12:57:58 -0300

    varios

--- src/lib/global.js
+++ src/lib/global.js
@@ -1,16 +1,16 @@
-//const connection_data = {
-//    user: "root",
-//    password: "root",
-//    host: "localhost",
-//    database: "optica_test17102023"//"optdelsol_28"//"optica_30"
-//} 
-
 const connection_data = {
-    user: "admin",
-    password: "admin2023",
-    host: "testdatabase.cogxzxmytpda.us-east-1.rds.amazonaws.com",
-    database: "optica_31_backup"
+    user: "root",
+    password: "root",
+    host: "localhost",
+    database: "optica_31_backup"//"optdelsol_28"//"optica_30"
 } 
+
+//const connection_data = {
+//    user: "admin",
+//    password: "admin2023",
+//    host: "testdatabase.cogxzxmytpda.us-east-1.rds.amazonaws.com",
+//    database: "optica_31_backup"
+//} 
 
 module.exports = {
     connection_data
