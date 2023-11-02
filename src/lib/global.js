00858f88ded1f786a83d9e6a0198bebe7501d68f
Bruno <ch.matias87@gmail.com>
2023-11-02 08:29:00 -0300

    varios

--- src/lib/global.js
+++ src/lib/global.js
@@ -1,16 +1,16 @@
-const connection_data = {
-    user: "root",
-    password: "root",
-    host: "localhost",
-    database: "optica_online_final"//"optdelsol_28"//"optica_30"
-} 
-
 //const connection_data = {
-//    user: "admin",
-//    password: "admin2023",
-//    host: "testdatabase.cogxzxmytpda.us-east-1.rds.amazonaws.com",
-//    database: "optica_31_backup"
+//    user: "root",
+//    password: "root",
+//    host: "localhost",
+//    database: "optica_online_final"//"optdelsol_28"//"optica_30"
 //} 
+
+const connection_data = {
+    user: "admin",
+    password: "admin2023",
+    host: "testdatabase.cogxzxmytpda.us-east-1.rds.amazonaws.com",
+    database: "optica_31_backup"
+} 
 
 module.exports = {
     connection_data
