b77de2b5cb38f045daf62331392cf0ce59a90f27
bruno <ch.matias87@gmail.com>
2024-03-25 22:07:06 -0300

    varios

---
+++ src/database/Anotacion.js
@@ -0,0 +1,15 @@
+const mysql_connection = require("../lib/mysql_connection")
+
+const agregarAnotacion = (params,callback) => {
+
+}
+
+const obtenerAnotacion = (idanotacion, callback) =>{
+
+}
+
+const obtenerAnotaciones = (callback) => {
+
+}
+
+module.exports = {agregarAnotacion,obtenerAnotacion,obtenerAnotaciones,}
---
+++ src/services/AnotacionService.js
@@ -0,0 +1,15 @@
+const AnotacionDB = require("../database/Anotacion")
+
+const agregarAnotacion = (params, callback) => {
+
+}
+
+const obtenerAnotacion = (idanotacion, callback) => {
+
+}
+
+const obtenerAnotaciones = (callback) => {
+
+}
+
+module.exports = {agregarAnotacion,obtenerAnotacion,obtenerAnotaciones}
