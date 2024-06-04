5080de01c5e00b3d1284cfbea3c276ab13675a74
Bruno <ch.matias87@gmail.com>
2024-06-04 13:01:04 -0300

    varios

--- src/database/Tag.js
+++ src/database/Tag.js
@@ -17,11 +17,11 @@ const eliminar_etiquetas = (data, callback) => {
 
 const agregar_codigo_tag = (data, callback) => {
     let values = ''
-    console.log(JSON.stringify(data))
+    
     data.codigos.forEach(c=>{
-        console.log("ksdfjk")
+        
         data.tags.forEach(t=>{
-            console.log("ddeee")
+            
             values += (values.length<1 ? '' : ',') + `(${c},'${t}')`
         })
     })
@@ -37,7 +37,7 @@ const agregar_codigo_tag = (data, callback) => {
 
 const agregar_tag = (data, callback) => {
     const query = `INSERT INTO tag (etiqueta, fk_categoria) VALUES ('${data.etiqueta}', ${+data.fkcategoria < -0 ? "NULL" : data.fkcategoria });`
-    console.log(query)
+    
     const connection = mysql_connection.getConnection()
     connection.connect()
     connection.query(query,(err,resp)=>{
@@ -50,7 +50,7 @@ const obtener_lista_tag = (data, callback) => {
 
     const query = `SELECT t.* FROM tag t WHERE (case when '${data.fkcategoria}'<>'-1' then t.fk_categoria = '${data.fkcategoria}' ELSE TRUE END ) ORDER BY t.etiqueta ASC;`
 
-    //console.log(query)
+    
     const connection = mysql_connection.getConnection()
     connection.connect()
     connection.query(query,(err,resp)=>{
@@ -61,7 +61,7 @@ const obtener_lista_tag = (data, callback) => {
 
 const agregar_categoria = (data,callback) => {
     const query = `INSERT INTO tag_categoria (fk_parent, nombre) VALUES (${+data.fkparent<0 ? "NULL" : data.fkparent}, '${data.nombre}');`
-    console.log(query)
+    
     const connection = mysql_connection.getConnection()
     connection.connect()
     connection.query(query,(err,resp)=>{
@@ -72,7 +72,7 @@ const agregar_categoria = (data,callback) => {
  
 const obtener_lista_categorias = (data,callback) => {
     const query = `SELECT * FROM tag_categoria;`
-    console.log(query)
+    
     const connection = mysql_connection.getConnection()
     connection.connect()
     connection.query(query,(err,resp)=>{
@@ -83,7 +83,7 @@ const obtener_lista_categorias = (data,callback) => {
  
 const obtener_lista_tag_codigo = (data,callback) => {
     const query = `SELECT cht.* FROM codigo_has_tag cht where cht.fk_codigo=${data.idcodigo};`
-    console.log(query)
+    
     const connection = mysql_connection.getConnection()
     connection.connect()
     connection.query(query,(err,resp)=>{
