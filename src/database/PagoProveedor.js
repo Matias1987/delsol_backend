ff01d96043e80c761e5e58c91f8702d963381fe4
Bruno <ch.matias87@gmail.com>
2024-07-15 12:56:50 -0300

    varios

---
+++ src/database/PagoProveedor.js
@@ -0,0 +1,24 @@
+const mysql_connection = require("../lib/mysql_connection")
+
+const agregar_pago_proveedor = (data,callback) => {
+
+}
+
+const lista_pago_proveedor = (data, callback) => {
+
+}
+
+const anular_pago_proveedor = (data,callback) => {
+
+}
+
+const detalle_pago_proveedor = (data, callback) => {
+
+}
+
+module.exports = {
+    agregar_pago_proveedor,
+    anular_pago_proveedor,
+    lista_pago_proveedor,
+    detalle_pago_proveedor,
+}
