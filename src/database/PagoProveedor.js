41a6efcf6e8086939638211e10d8a08842eebb6d
Bruno <ch.matias87@gmail.com>
2024-07-16 13:00:03 -0300

    varjios

--- src/database/PagoProveedor.js
+++ src/database/PagoProveedor.js
@@ -2,6 +2,30 @@ const mysql_connection = require("../lib/mysql_connection")
 
 const agregar_pago_proveedor = (data,callback) => {
 
+    const connection = mysql_connection.getConnection()
+
+    connection.connect()
+
+     const query_pago = `INSERT INTO pago_proveedor (fk_proveedor, monto) VALUES (${data.fk_proveedor}, ${data.monto});`
+
+    connection.query(query_pago,(err,resp)=>{
+
+        //go thru mp
+        let mp=''
+        data.mp.forEach(mp=>{
+            mp+= (mp.length>0?',':'') +`('${mp.modo_pago}',${resp.inserId},${mp.monto})`
+        })
+
+        const query_modo = `INSERT INTO pago_proveedor_modo (modo_pago, fk_pago_proveedor, monto) VALUES ${mp};`
+        
+        connection.query(query_modo,(_resp)=>{
+            callback(_resp)
+        })
+
+        connection.end()
+
+    })
+
 }
 
 const lista_pago_proveedor = (data, callback) => {
