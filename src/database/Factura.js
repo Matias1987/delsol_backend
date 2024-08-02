7d66e2c3c8ea6583fd5fbe13e44b1971db5a3d02
Bruno <ch.matias87@gmail.com>
2024-08-02 12:58:07 -0300

    varios

--- src/database/Factura.js
+++ src/database/Factura.js
@@ -60,9 +60,70 @@ const lista_elementos_factura = (data, callback) => {
     connection.end();
 }
 
+const agregar_factura_v2 = (data, callback) => {
+    console.log(JSON.stringify(data))
+    let query_factura = `INSERT INTO factura (numero, proveedor_idproveedor, monto, cantidad, tipo, punto_venta) VALUES ('${data.nro}', ${data.fkproveedor},${data.total},${0},'${data.tipo}','${data.puntoVenta}');` //toDo
+    let query_iva = `INSERT INTO factura_iva (fk_factura, monto) VALUES `
+    let query_retenciones = `INSERT INTO factura_retencion (fk_factura, monto) VALUES `
+    let query_percepciones = `INSERT INTO factura_percepcion (fk_factura, monto) VALUES `
+    let idfactura=-1;
+
+    const _process = (_queries) => {
+        if(_queries.length<1)
+        {
+            connection.end()
+            callback(idfactura)
+            return
+        }
+        const query = _queries.pop()
+        connection.query(query,(err,resp)=>{
+            _process(queries)
+        })
+    }
+
+    //first insert factura
+    connection.query(query_factura,(err,resp)=>{
+        
+        idfactura = resp.insertId;
+        
+        params
+        .iva
+        .forEach(row=>{query_factura += (query_factura.length>0?',':'') + `(${idfactura}, ${row.monto})`});
+
+        params
+        .retenciones
+        .forEach(row=>{query_retenciones += (query_retenciones.length>0?',':'') + `(${idfactura}, ${row.monto})`});
+
+        params
+        .percepciones
+        .forEach(row=>{query_percepciones += (query_percepciones.length>0?',':'') + `(${idfactura}, ${row.monto})`});
+
+        let queries=[]
+
+        if(params.iva.length>0)
+        {
+            queries.push(query_iva)
+        }
+
+        if(params.retenciones.length>0)
+        {
+            queries.push(query_retenciones)
+        }
+
+        if(params.percepciones.length>0)
+        {
+            queries.push(query_percepciones)
+        }
+        
+        _process(queries)
+
+    })
+}
+
 module.exports = {
     obtener_facturas,
     agregar_factura,
     detalle_factura,
     lista_elementos_factura,
+    agregar_factura_v2,
 }
