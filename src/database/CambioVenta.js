3ca59811185a5e667db09f56b0c0b3f69ea31c62
Bruno <ch.matias87@gmail.com>
2024-11-22 12:59:42 -0300

    cambio venta

--- src/database/CambioVenta.js
+++ src/database/CambioVenta.js
@@ -2,7 +2,38 @@ const mysql_connection = require("../lib/mysql_connection")
 
 const registrar_cambio_venta_item = (data, callback) => {
     const connection = mysql_connection.getConnection()
-    const query = ``
+    const query = `INSERT INTO venta_has_stock (
+                    venta_idventa, 
+                    stock_sucursal_idsucursal, 
+                    stock_codigo_idcodigo,
+                    tipo,
+                    esf, 
+                    cil, 
+                    eje, 
+                    curva_base, 
+                    diametro,
+                    precio, 
+                    total,
+                    orden
+                    )
+                    VALUE(
+                    ${connection.escape(data.idventa)},
+                    ${connection.escape(data.idventa)},
+                    ${connection.escape(data.idventa)},
+                    ${connection.escape(data.idventa)},
+                    ${connection.escape(data.idventa)},
+                    ${connection.escape(data.idventa)},
+                    ${connection.escape(data.idventa)},
+                    ${connection.escape(data.idventa)},
+                    ${connection.escape(data.idventa)},
+                    ${connection.escape(data.idventa)},
+                    ${connection.escape(data.idventa)},
+                    ${connection.escape(data.idventa)},
+                    )`
+
+    const query1 = `UPDATE venta_has_stock vhs SET vhs.activo =${data.idventa} WHERE vhs.tipo='${data.idventa}' AND vhs.venta_idventa=${data.idventa};`
+
+
 
     connection.connect()
 
