3044fbe7ebc3651ea0375d5cbedbb87098875803
Bruno <ch.matias87@gmail.com>
2025-04-15 13:03:40 -0300

    varios

--- src/database/ObjetivoSucursal.js
+++ src/database/ObjetivoSucursal.js
@@ -35,7 +35,9 @@ const obtener_objetivo_sucursal = (data, callback) => {
 
 const establecer_objetivo_sucursal = (data, callback) => {
     const connection = mysql_connection.getConnection()
+    console.log(JSON.stringify(data))
     const query = `INSERT INTO objetivo_sucursal (fk_sucursal, monto) VALUES (${connection.escape(data.idsucursal)},${connection.escape(data.monto)});`
+    console.log(query)
     connection.connect();
 
     connection.query(query,(err,response)=>{
@@ -55,7 +57,7 @@ const obtener_progreso_sucursal_objetivo = (data, callback) => {
                         SUM(v.monto_total) AS 'monto'
                         FROM venta v 
                         WHERE 
-                        v.sucursal_idsucursal=8 AND  
+                        v.sucursal_idsucursal=${connection.escape(data.fksucursal)}  AND  
                         v.estado='ENTREGADO' AND 
                         MONTH(v.fecha_retiro) = ${connection.escape(data.mes)} AND
                         YEAR(v.fecha_retiro)= ${connection.escape(data.anio)}
