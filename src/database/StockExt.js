0a74b8c71718f708842b6cba72958a56a9416957
Bruno <ch.matias87@gmail.com>
2025-12-18 12:56:27 -0300

    varios

--- src/database/StockExt.js
+++ src/database/StockExt.js
@@ -1,13 +1,17 @@
 const { doQuery } = require("./helpers/queriesHelper");
 
 const agregarStock = ({codigo, descripcion, precio, idsubgrupo},callback) =>{
-    const query1 = `insert into codigo (subgrupo_idsubgrupo, codigo, descripcion, precio, modo_precio) values ('${idsubgrupo}','${codigo}','${descripcion}','${precio}',1)`;
+    const query1 = `insert ignore into codigo (subgrupo_idsubgrupo, codigo, descripcion, precio, modo_precio) values ('${idsubgrupo}','${codigo}','${descripcion}','${precio}',1)`;
 
     doQuery(query1,(response)=>{
-        console.log(`INSERT INTO stock (sucursal_idsucursal, codigo_idcodigo, cantidad) (SELECT s0.idsucursal,${response.data.insertId},0 FROM sucursal s0);`);
-        doQuery(`INSERT INTO stock (sucursal_idsucursal, codigo_idcodigo, cantidad) (SELECT s0.idsucursal,${response.data.insertId},0 FROM sucursal s0);`,
+        //console.log(`INSERT INTO stock (sucursal_idsucursal, codigo_idcodigo, cantidad) (SELECT s0.idsucursal,${response.data.insertId},0 FROM sucursal s0);`);
+        doQuery(`INSERT ignore INTO stock (sucursal_idsucursal, codigo_idcodigo, cantidad) (SELECT s0.idsucursal,${response.data.insertId},0 FROM sucursal s0);`,
             (_response)=>{
-                console.log(JSON.stringify(response.data))
+                if (response.data.warningStatus > 0) {
+                    console.log('Duplicate entry ignored, warning issued.');
+                    return callback({idcodigo:-1, codigo: '', msg:"DUPLICATED"})
+                }
+                //console.log(JSON.stringify(response.data))
                 return callback({idcodigo:response.data.insertId, codigo: codigo, msg:"Stock added...."})
             }
         )
